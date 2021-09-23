1331037e479321f7bf1b54338d70130cd0dd07f0
Felipe <felipe@pop-os.localdomain>
2021-09-23 11:37:02 -0300

    Create table of evento

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from core.models import Evento
 
 # Register your models here.
+
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ("title", "data_evento", "data_criacao")  # Campos que devem
+    # aparecer no meu evento
+    list_filter = (
+        "title",
+        "usuario",
+    )
+
+
+admin.site.register(Evento, EventoAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import (
+    User,
+)  # Importa a tabela User criada pelo django
 
 # Create your models here.
+
+
+class Evento(models.Model):
+    title = models.CharField(max_length=100)  # Max de 100 caractere
+    description = models.TextField(
+        blank=True, null=True
+    )  # Pode ser branco e nulo
+    data_evento = models.DateTimeField(
+        verbose_name="data do evento"
+    )  # nome da coluna
+    data_criacao = models.DateTimeField(
+        auto_now=True
+    )  # Dato automatico, vai inserir o momento atual
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # Para mudar o nome da tabela
+    # class Meta: db_table = 'evento'
+    def __str__(self) -> str:
+        return self.title
